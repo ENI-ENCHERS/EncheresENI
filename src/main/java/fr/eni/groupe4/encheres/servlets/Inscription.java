0036a56a0672c3234e3ebfc144fa2055437f462f
DatFrenchNerd <kenny.marmin@gmail.com>
2021-05-20 19:52:08 +0200

    Mises à jour pour la fonctionnalité d'inscription
    - Création de l'objet FormulaireInscription
    - Mise à jour de la servlet Inscription
    - Mise à jour du web.xml

--- src/main/java/fr/eni/groupe4/encheres/servlets/Inscription.java
+++ src/main/java/fr/eni/groupe4/encheres/servlets/Inscription.java
@@ -7,22 +7,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.groupe4.encheres.ihm.FormulaireInscription;
+import fr.eni.groupe4.encheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class GestionInscription
  */
-@WebServlet(description = "Gere la page d'inscription", urlPatterns = { "/GestionInscription" })
+@WebServlet(description = "Gere la page d'inscription", urlPatterns = { "/inscription" })
 public class Inscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	public static final String ATT_USER = "utilisateur";
+	public static final String ATT_FORM = "form";
 	public static final String VUE = "/WEB-INF/encheres/inscription.jsp";
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* On affiche la page d'inscription */
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
-	}
-	
-	public void doPost() {
-		
-	}
+	    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+	        /* Affichage de la page d'inscription */
+	        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	    }
 
+	    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+	        /* Préparation de l'objet formulaire */
+	        FormulaireInscription form = new FormulaireInscription();
+
+	        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+	        Utilisateur utilisateur = form.inscrireUtilisateur( request );
+
+	        /* Stockage du formulaire et du bean dans l'objet request */
+	        request.setAttribute( ATT_FORM, form );
+	        request.setAttribute( ATT_USER, utilisateur );
+
+	        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	    }
 }
