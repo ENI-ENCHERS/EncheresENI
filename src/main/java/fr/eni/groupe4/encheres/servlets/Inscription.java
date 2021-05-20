f1dbfde55b45455d99a41ff29bdec6bf07a84b65
DatFrenchNerd <kenny.marmin@gmail.com>
2021-05-20 16:19:34 +0200

    Refactor FormulaireInscription

--- src/main/java/fr/eni/groupe4/encheres/servlets/Inscription.java
+++ src/main/java/fr/eni/groupe4/encheres/servlets/Inscription.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class GestionInscription
  */
 @WebServlet(description = "Gere la page d'inscription", urlPatterns = { "/GestionInscription" })
-public class FormulaireInscription extends HttpServlet {
+public class Inscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public static final String VUE = "/WEB-INF/encheres/inscription.jsp";
